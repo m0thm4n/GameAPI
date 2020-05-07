d380d3759e74147b27725f93a44590b81f1be8e2
blake.e.kendall@gmail.com <blake.e.kendall@gmail.com>
2020-05-07 18:21:41 -0400

    edited XboxPostModel

--- Game.Models/XboxModels/XboxPostModel.cs
+++ Game.Models/XboxModels/XboxPostModel.cs
@@ -5,11 +5,15 @@ namespace Game.Models.XboxModels
     public class XboxPostModel
     {
         [Required]
+        [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
+        [MaxLength(100, ErrorMessage = "There are too many characters in this field.")]
         public string Name { get; set; }
+
         public double Price { get; set; }
         public double Rating { get; set; }
         public string MaturityRating { get; set; }
         public string Genre { get; set; }
+        
 
     }
 }
