00dee5c370c5e4f8b7a5b85fa8a787858a6d13fd
Nathan Moritz <nateim3@gmail.com>
2020-05-11 21:06:56 -0400

    Refactored service and contract

--- Game.Services/PlaystationService.cs
+++ Game.Services/PlaystationService.cs
@@ -1,12 +1,69 @@
-﻿using System;
+﻿using Game.API.Models;
+using Game.Contracts;
+using Game.Data.Entites.PlaystationGame;
+using Game.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Policy;
 
 namespace Game.Services
 {
-    class PlaystationService
+    class PlaystationService : IPlaystationGameService
     {
+        public void CreateGame(PlaystationGameCreateModel playstaionGameToCreate)
+        {
+            var entity = new PlaystationGame()
+            {
+                PlaystationGameId = playstaionGameToCreate.PlaystationGameId,
+                Name = playstaionGameToCreate.Name,
+                Developer = playstaionGameToCreate.Developer,
+                Publisher = playstaionGameToCreate.Publisher,
+                Rating = playstaionGameToCreate.Rating,
+                MaturityRating = playstaionGameToCreate.MaturityRating,
+                Price = playstaionGameToCreate.Price
+            };
+
+            using (ApplicationDbContext context = new ApplicationDbContext())
+            {
+                context.PlaystationGames.Add(entity);
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteGame(int id)
+        {
+            using (ApplicationDbContext context = new ApplicationDbContext())
+            {
+                var entity = context.PlaystationGames.Find(id);
+                context.PlaystationGames.Remove(entity);
+                context.SaveChanges();
+            }
+        }
+
+        public PlaystationGameItem GetGame(int id)
+        {
+            using (ApplicationDbContext context = new ApplicationDbContext())
+            {
+                var entity = context.PlaystationGames.Find(id);
+                return entity;
+            }
+        }
+
+        public IEnumerable<PlaystationGameItemList> GetGames()
+        {
+            using (ApplicationDbContext context = new ApplicationDbContext())
+            {
+                var entities = context.PlaystationGames.ToList();
+                return entities;
+            }
+        }
+
+        public void UpdateGame(int id)
+        {
+            using (ApplicationDbContext context = new ApplicationDbContext())
+            {
+                var entities = context.PlaystationGames.Find(id);
+            }
+        }
     }
-}
