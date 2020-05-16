38392e79c556e6ac08642c154fbc341b99c07501
Nathan Moritz <nateim3@gmail.com>
2020-05-16 12:32:50 -0400

    Added migration and PlaystationGame Controller

---
+++ Game.Data/Migrations/202005161619297_InitialCreate.cs
@@ -0,0 +1,183 @@
+﻿namespace Game.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.PCGames",
+                c => new
+                    {
+                        PCGameId = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false),
+                        Developer = c.String(nullable: false),
+                        Publisher = c.String(nullable: false),
+                        Genre = c.String(nullable: false),
+                        Rating = c.Double(nullable: false),
+                        MaturityRating = c.String(nullable: false),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.PCGameId);
+            
+            CreateTable(
+                "dbo.PlaystationGames",
+                c => new
+                    {
+                        PlaystationGameId = c.Int(nullable: false, identity: true),
+                        Title = c.String(nullable: false),
+                        Genre = c.String(nullable: false),
+                        Rating = c.Double(nullable: false),
+                        MaturityRating = c.String(nullable: false),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        DeveloperId = c.Int(nullable: false),
+                        PublisherId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.PlaystationGameId)
+                .ForeignKey("dbo.Developers", t => t.DeveloperId, cascadeDelete: true)
+                .ForeignKey("dbo.Publishers", t => t.PublisherId, cascadeDelete: true)
+                .Index(t => t.DeveloperId)
+                .Index(t => t.PublisherId);
+            
+            CreateTable(
+                "dbo.Developers",
+                c => new
+                    {
+                        DeveloperId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.DeveloperId);
+            
+            CreateTable(
+                "dbo.Publishers",
+                c => new
+                    {
+                        PublisherId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.PublisherId);
+            
+            CreateTable(
+                "dbo.IdentityRoles",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.IdentityUserRoles",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(),
+                        IdentityRole_Id = c.String(maxLength: 128),
+                        ApplicationUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.UserId)
+                .ForeignKey("dbo.IdentityRoles", t => t.IdentityRole_Id)
+                .ForeignKey("dbo.ApplicationUsers", t => t.ApplicationUser_Id)
+                .Index(t => t.IdentityRole_Id)
+                .Index(t => t.ApplicationUser_Id);
+            
+            CreateTable(
+                "dbo.ApplicationUsers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Email = c.String(),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.IdentityUserClaims",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                        ApplicationUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.ApplicationUsers", t => t.ApplicationUser_Id)
+                .Index(t => t.ApplicationUser_Id);
+            
+            CreateTable(
+                "dbo.IdentityUserLogins",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        LoginProvider = c.String(),
+                        ProviderKey = c.String(),
+                        ApplicationUser_Id = c.String(maxLength: 128),
+                    })
+                .PrimaryKey(t => t.UserId)
+                .ForeignKey("dbo.ApplicationUsers", t => t.ApplicationUser_Id)
+                .Index(t => t.ApplicationUser_Id);
+            
+            CreateTable(
+                "dbo.XboxGames",
+                c => new
+                    {
+                        XboxId = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Genre = c.String(nullable: false),
+                        Rating = c.Double(nullable: false),
+                        MaturityRating = c.String(nullable: false),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        DeveloperId = c.Int(nullable: false),
+                        PublisherId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.XboxId)
+                .ForeignKey("dbo.Developers", t => t.DeveloperId, cascadeDelete: true)
+                .ForeignKey("dbo.Publishers", t => t.PublisherId, cascadeDelete: true)
+                .Index(t => t.DeveloperId)
+                .Index(t => t.PublisherId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.XboxGames", "PublisherId", "dbo.Publishers");
+            DropForeignKey("dbo.XboxGames", "DeveloperId", "dbo.Developers");
+            DropForeignKey("dbo.IdentityUserRoles", "ApplicationUser_Id", "dbo.ApplicationUsers");
+            DropForeignKey("dbo.IdentityUserLogins", "ApplicationUser_Id", "dbo.ApplicationUsers");
+            DropForeignKey("dbo.IdentityUserClaims", "ApplicationUser_Id", "dbo.ApplicationUsers");
+            DropForeignKey("dbo.IdentityUserRoles", "IdentityRole_Id", "dbo.IdentityRoles");
+            DropForeignKey("dbo.PlaystationGames", "PublisherId", "dbo.Publishers");
+            DropForeignKey("dbo.PlaystationGames", "DeveloperId", "dbo.Developers");
+            DropIndex("dbo.XboxGames", new[] { "PublisherId" });
+            DropIndex("dbo.XboxGames", new[] { "DeveloperId" });
+            DropIndex("dbo.IdentityUserLogins", new[] { "ApplicationUser_Id" });
+            DropIndex("dbo.IdentityUserClaims", new[] { "ApplicationUser_Id" });
+            DropIndex("dbo.IdentityUserRoles", new[] { "ApplicationUser_Id" });
+            DropIndex("dbo.IdentityUserRoles", new[] { "IdentityRole_Id" });
+            DropIndex("dbo.PlaystationGames", new[] { "PublisherId" });
+            DropIndex("dbo.PlaystationGames", new[] { "DeveloperId" });
+            DropTable("dbo.XboxGames");
+            DropTable("dbo.IdentityUserLogins");
+            DropTable("dbo.IdentityUserClaims");
+            DropTable("dbo.ApplicationUsers");
+            DropTable("dbo.IdentityUserRoles");
+            DropTable("dbo.IdentityRoles");
+            DropTable("dbo.Publishers");
+            DropTable("dbo.Developers");
+            DropTable("dbo.PlaystationGames");
+            DropTable("dbo.PCGames");
+        }
+    }
+}
