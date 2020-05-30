de778900c0ee31bd23f6246ec13560e3503be546
blake.e.kendall@gmail.com <blake.e.kendall@gmail.com>
2020-05-30 11:40:30 -0400

    fixed PcUpdateModel with code from Kyle

--- Game.Models/PC/PCUpdateModel.cs
+++ Game.Models/PC/PCUpdateModel.cs
@@ -2,6 +2,8 @@
 {
     public class PCUpdateModel
     {
+        public int PCGameId { get; set; }
+        public string Title { get; set; }
         public decimal Price { get; set; }
         public double Rating { get; set; }
     }
