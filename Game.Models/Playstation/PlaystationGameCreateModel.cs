ed2f4d23cedbaddfd7910f930b1969c64d608109
Nathan Moritz <nateim3@gmail.com>
2020-06-04 17:51:10 -0400

    Updated CreateModel

--- Game.Models/Playstation/PlaystationGameCreateModel.cs
+++ Game.Models/Playstation/PlaystationGameCreateModel.cs
@@ -8,5 +8,7 @@
         public double Rating { get; set; }
         public string MaturityRating { get; set; }
         public decimal Price { get; set; }
+        public int PublisherId { get; set; }
+        public int DeveloperId { get; set; }
     }
 }
